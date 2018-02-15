f4a75dedc3842340f01df051c0582babc51ab41f
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-15 15:39:26 +0700

    add new route for category

--- application/admin/routes/web.php
+++ application/admin/routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
+// Login Routes
 Auth::routes();
-Route::get('/add_user', 'Auth\RegisterController@showRegistrationForm')->name('add_user');
-Route::get('/user', 'User\UserListController@show')->name('user');
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
+    //User Routes
+    Route::get('/add_user', 'Auth\RegisterController@showRegistrationForm')->name('add_user');
+    Route::get('/user', 'User\UserListController@show')->name('user');
+    // Default Home
+    Route::get('/', 'HomeController@index')->name('home');
+    // Categories Route
+    Route::resource('categories', CategoriesController::class);
+    Route::get('/add_categories', 'CategoriesController@addCategories')->name('add_categories');
+    Route::post('/delete_categories/{categories}', 'CategoriesController@delete')->name('categories.delete');
+});
