750dd47fccaca9761b18ebcbcead5788f06a8649
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-21 20:54:27 +0700

    update index public

--- public/index.php
+++ public/index.php
@@ -6,38 +6,41 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
 define('LARAVEL_START', microtime(true));
 
 /*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-*/
+  |--------------------------------------------------------------------------
+  | Register The Auto Loader
+  |--------------------------------------------------------------------------
+  |
+ */
 
-require __DIR__.'/../application/admin/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-*/
-
-$app = require_once __DIR__.'/../application/admin/bootstrap/app.php';
+require __DIR__ . '/../application/admin/vendor/autoload.php';
 
 /*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-*/
+  |--------------------------------------------------------------------------
+  | Turn On The Lights
+  |--------------------------------------------------------------------------
+  |
+ */
+
+$app = require_once __DIR__ . '/../application/admin/bootstrap/app.php';
+
+/*
+  |--------------------------------------------------------------------------
+  | Run The Application
+  |--------------------------------------------------------------------------
+  |
+ */
+//
+//$app->bind('path.public', function() {
+//    return __DIR__;
+//});
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+        $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
