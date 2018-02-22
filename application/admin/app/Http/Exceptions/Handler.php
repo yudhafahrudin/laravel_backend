3ea43033d77d9467675a4efeef8a115234ec2438
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-22 09:38:56 +0700

    fixing error handle by me hehehe

--- application/admin/app/Http/Exceptions/Handler.php
+++ application/admin/app/Http/Exceptions/Handler.php
@@ -47,9 +47,9 @@ class Handler extends ExceptionHandler {
      */
     public function render($request, Exception $exception) {
         
-        if ($e instanceof NotFoundHttpException) {
-            return response()->view('missing', [], 404);
-        }
+//        if ($exception instanceof NotFoundHttpException) {
+//            return response()->view('missing', [], 404);
+//        }
 
         return parent::render($request, $exception);
     }
