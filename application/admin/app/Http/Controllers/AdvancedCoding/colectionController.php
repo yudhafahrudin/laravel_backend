96cecbc688108e46f7925f861e5f176243fcee28
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-24 20:05:20 +0700

    add collection for improve code

---
+++ application/admin/app/Http/Controllers/AdvancedCoding/colectionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\AdvancedCoding;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class colectionController extends Controller {
+
+    public function __construct() {
+        
+    }
+
+    public function inviteLoop() {
+        $interval = 60; //minutes
+        set_time_limit(0);
+        $sleep = $interval * 60 - (time());
+
+        while (1) {
+            if (time() != $sleep) {
+                // the looping will pause on the specific time it was set to sleep
+                // it will loop again once it finish sleeping.
+                time_sleep_until($sleep);
+            }
+            #do the routine job, trigger a php function and what not.
+        }
+    }
+
+}
