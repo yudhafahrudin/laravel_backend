cccb7585df80b34d018c29e77709e9a52aa8343b
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-24 20:07:35 +0700

    add function for post ajax

--- application/admin/resources/views/layouts/footer.blade.php
+++ application/admin/resources/views/layouts/footer.blade.php
@@ -1,8 +1,30 @@
 @section('footer')
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('material/admin/js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{ asset('material/admin/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('material/admin/js/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('material/admin/js/dropify.min.js') }}"></script>
+<script src="{{ asset('material/admin/js/jquery-confirm.js') }}"></script>
 <script src="{{ asset('material/admin/js/custom.js') }}"></script>
+
+<script type="text/javascript">
+function profileAJAX(username) {
+    $.ajax({
+        url: "{{url('user/profile/')}}/" + username,
+        dataType: 'text',
+        type: 'get',
+        contentType: 'application/x-www-form-urlencoded',
+        data: {'username': username},
+        success: function (data, textStatus, jQxhr) {
+            $('.modal-body').html(data);
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+</script>
+
 @endsection
