1d08887401a45822a2ad2fb5f43455bb233f1cab
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-24 20:07:52 +0700

    add jquery confirmation

--- public/material/admin/js/custom.js
+++ public/material/admin/js/custom.js
@@ -2,29 +2,35 @@
 //		});
 
 (window.jQuery);
-	$(window).on('resize', function () {
-  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+$(window).on('resize', function () {
+    if ($(window).width() > 768)
+        $('#sidebar-collapse').collapse('show')
 })
 $(window).on('resize', function () {
-  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+    if ($(window).width() <= 767)
+        $('#sidebar-collapse').collapse('hide')
 })
 
-$(document).on('click', '.panel-heading span.clickable', function(e){
+$(document).on('click', '.panel-heading span.clickable', function (e) {
     var $this = $(this);
-	if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.addClass('panel-collapsed');
-		$this.find('em').removeClass('fa-toggle-up').addClass('fa-toggle-down');
-	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.removeClass('panel-collapsed');
-		$this.find('em').removeClass('fa-toggle-down').addClass('fa-toggle-up');
-	}
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('em').removeClass('fa-toggle-up').addClass('fa-toggle-down');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('em').removeClass('fa-toggle-down').addClass('fa-toggle-up');
+    }
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#myTable').DataTable();
 });
-$(document).ready(function(){
+$(document).ready(function () {
     $('.dropify').dropify();
 });
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+})
