f7444eaa2f755ddd7a7b2ca9299bcf1769f73d11
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-24 20:12:54 +0700

    add route for breadcums

--- application/admin/routes/web.php
+++ application/admin/routes/web.php
@@ -20,12 +20,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     //User Routes
     Route::get('/user/profile/{username}', 'User\UserController@showProfileUser')->name('user.profile');
-//    Route::get('/user/profile/{username}', function(){
-//        return view('user.profile');
-//    })->name('user.profile');
-//    
-//    Route::put('/user/{id}/edit', 'User\UserController@showProfileUser')->name('edit.user');
-//    
     Route::get('/user/add', 'Auth\RegisterController@showRegistrationForm')->name('add_user');
     Route::get('/user', 'User\UserListController@show')->name('show.user');
     Route::get('/user/trash/all', 'User\UserTrashController@show')->name('show.trash.user');
@@ -50,12 +44,27 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/logs/restored/show/{id}', 'LogsController@showDetailLogsRestored')->name('logs.detail.restored');
     Route::get('/logs/updated/show/{id}/{type}/', 'LogsController@showDetailLogs')->name('logs.detail.updated');
     Route::get('/logs/deleted/show/{id}', 'LogsController@showDetailLogsDeleted')->name('logs.detail.deleted');
-    
+
     //Direct image route
     Route::get('images/profile/{username}/{date}/{id}/{type}/{file}', function($username, $date, $id, $type, $file) {
         // Check if file exists in app/storage/file folder
         $file_path = 'storage/user/images/profile/' . $username . '/' . $date . '/' . $id . '/' . $type . '/' . $file;
-//        return response(url($file_path), 200)->header('Content-Type', 'text/plain');
+        // Return response(url($file_path), 200)->header('Content-Type', 'text/plain');
         return Image::make($file_path)->response();
     });
+});
+
+//Breadcrumbs side
+Breadcrumbs::register('home', function ($breadcrumbs) {
+    $breadcrumbs->push('home', route('home'));
+});
+
+Breadcrumbs::register('user', function ($breadcrumbs) {
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push('user', route('show.user'));
+});
+
+Breadcrumbs::register('user.profile', function ($breadcrumbs, $user) {
+    $breadcrumbs->parent('user');
+    $breadcrumbs->push($user->username, route('user.profile', $user->username));
 });
