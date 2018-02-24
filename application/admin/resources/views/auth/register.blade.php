8a3c7054fe9688059cf1cc37459b0c2246d8564b
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-02-24 20:09:23 +0700

    add laravel breadcumb package

--- application/admin/resources/views/auth/register.blade.php
+++ application/admin/resources/views/auth/register.blade.php
@@ -3,7 +3,10 @@
 
 @section('content')
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-    @yield('breadcumb')
+<div class="row">
+        {{ Breadcrumbs::render('user') }}
+        <br>
+    </div>
     <div class="row">
         <div class="col-md-12">
 
