0e0fa1c551c15b7dfe036005f401076546602398
Yuda <yuda@twiscode.com>
2018-04-13 13:56:37 +0700

    update some controller

---
+++ application/admin/app/Transformers/CategorieRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface CategorieRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface CategorieRepository extends RepositoryInterface
+{
+    //
+}
