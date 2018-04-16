0cd360a90feabb0d954a5bbac4f5e267a9cd63d2
Yuda <yuda@twiscode.com>
2018-04-16 17:39:05 +0700

    Added time for seed

--- application/admin/database/seeds/DatabaseSeeder.php
+++ application/admin/database/seeds/DatabaseSeeder.php
@@ -3,21 +3,23 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UsersTableSeeder::class);
-          DB::table('users')->insert([
+        DB::table('users')->insert([
             'name' => str_random(10),
-              'username'=> 'superadmin',
-            'email' => str_random(10).'@gmail.com',
+            'username' => 'superadmin',
+            'email' => str_random(10) . '@gmail.com',
             'password' => bcrypt('admin123'),
+            'created_at' => \Carbon::parse('1996-02-26'),
+            'updated_at' => \Carbon::parse('1996-02-26')
         ]);
     }
+
 }
