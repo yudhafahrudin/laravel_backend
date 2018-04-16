e03e1feb635948eef643d9c623e37d672ba1899b
Yuda <yuda@twiscode.com>
2018-04-16 17:39:58 +0700

    Add prefix and group in route

--- application/admin/routes/web.php
+++ application/admin/routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-  |--------------------------------------------------------------------------
-  | Web Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register web routes for your application. These
-  | routes are loaded by the RouteServiceProvider within a group which
-  | contains the "web" middleware group. Now create something great!
-  |
- */
 
 // Login Routes
 Auth::routes();
@@ -18,32 +8,42 @@ Route::middleware(['auth'])->group(function () {
 
     // Default Home
     Route::get('/', 'HomeController@index')->name('home');
+
     //User Routes
-    Route::get('/user/profile/{username}', 'User\UserController@showProfileUser')->name('user.profile');
-    Route::get('/user/add', 'Auth\RegisterController@showRegistrationForm')->name('add_user');
-    Route::get('/user', 'User\UserListController@show')->name('show.user');
-    Route::get('/user/trash/all', 'User\UserTrashController@show')->name('show.trash.user');
-    Route::get('/user/show/{id}', 'User\UserController@showDetailUser')->name('show.user.detail');
-    Route::get('/user/edit/{id}', 'User\UserController@showEdit')->name('show.edit.user');
-    Route::post('/user/{id}/delete', 'User\UserController@delete')->name('delete.user');
-    Route::get('/user/{id}/restore', 'User\UserTrashController@restore')->name('restore.user');
-    Route::put('/user/{id}/edit', 'User\UserController@edit')->name('edit.user');
-    Route::delete('/user/{id}/destroy', 'User\UserTrashController@destroy')->name('destroy.user');
+    Route::prefix('users')->group(function () {
+        Route::namespace('User')->group(function () {
+            Route::get('/', 'UserListController@show')->name('show.user');
+            Route::get('/trash/all', 'UserTrashController@show')->name('show.trash.user');
+            Route::get('/show/{id}', 'UserController@showDetailUser')->name('show.user.detail');
+            Route::get('/edit/{id}', 'UserController@showEdit')->name('show.edit.user');
+            Route::post('/{id}/delete', 'UserController@delete')->name('delete.user');
+            Route::get('/{id}/restore', 'UserTrashController@restore')->name('restore.user');
+            Route::put('/{id}/edit', 'UserController@edit')->name('edit.user');
+            Route::delete('/{id}/destroy', 'UserTrashController@destroy')->name('destroy.user');
+            Route::get('/profile/{username}', 'UserController@showProfileUser')->name('user.profile');
+        });
+        Route::get('/add', 'Auth\RegisterController@showRegistrationForm')->name('add_user');
+    });
+
     // Categories Routes
     Route::resource('categories', CategoriesController::class);
     Route::get('/add_categories', 'CategoriesController@addCategories')->name('add_categories');
     Route::post('/delete_categories/{categories}', 'CategoriesController@delete')->name('categories.delete');
-    // Products Routes
-    Route::resource('products', ProductsController::class);
+
+
     //Logs Routes
-    Route::get('/logs/created', 'LogsController@logCreated')->name('logs.created');
-    Route::get('/logs/restored', 'LogsController@logRestored')->name('logs.restored');
-    Route::get('/logs/updated', 'LogsController@logUpdated')->name('logs.updated');
-    Route::get('/logs/deleted', 'LogsController@logDeleted')->name('logs.deleted');
-    Route::get('/logs/created/show/{id}', 'LogsController@showDetailLogsCreated')->name('logs.detail.created');
-    Route::get('/logs/restored/show/{id}', 'LogsController@showDetailLogsRestored')->name('logs.detail.restored');
-    Route::get('/logs/updated/show/{id}/{type}/', 'LogsController@showDetailLogs')->name('logs.detail.updated');
-    Route::get('/logs/deleted/show/{id}', 'LogsController@showDetailLogsDeleted')->name('logs.detail.deleted');
+    Route::prefix('logs')->group(function () {
+        Route::get('/created', 'LogsController@logCreated')->name('logs.created');
+        Route::get('/restored', 'LogsController@logRestored')->name('logs.restored');
+        Route::get('/updated', 'LogsController@logUpdated')->name('logs.updated');
+        Route::get('/deleted', 'LogsController@logDeleted')->name('logs.deleted');
+        Route::get('/created/show/{id}', 'LogsController@showDetailLogsCreated')->name('logs.detail.created');
+        Route::get('/restored/show/{id}', 'LogsController@showDetailLogsRestored')->name('logs.detail.restored');
+        Route::get('/updated/show/{id}/{type}/', 'LogsController@showDetailLogs')->name('logs.detail.updated');
+        Route::get('/deleted/show/{id}', 'LogsController@showDetailLogsDeleted')->name('logs.detail.deleted');
+    });
+    //Testing
+    Route::get('/test/mkdir', 'AdvancedCoding\colectionController@mkdir');
 
     //Direct image route
     Route::get('images/profile/{username}/{date}/{id}/{type}/{file}', function($username, $date, $id, $type, $file) {
