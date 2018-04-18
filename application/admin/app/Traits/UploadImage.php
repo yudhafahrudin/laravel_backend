38715202bc612eb29308baf1e19a5a73aae165a5
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-04-18 07:43:49 +0700

    cleaning code of upload image

--- application/admin/app/Traits/UploadImage.php
+++ application/admin/app/Traits/UploadImage.php
@@ -16,41 +16,50 @@ trait UploadImage {
     protected $pathImage;
     protected $pathImageTemp;
     protected $pathQuality = [];
+    protected $widthSize = 200;
+    protected $heightSize = 200;
+    protected $backgroundCanvas;
 
     public function saveImage($request) {
         $photo = $request->file('photo');
+
+        $this->backgroundCanvas = \Image::canvas($this->widthSize, $this->heightSize);
 
         // File named
         $imagename = time() . '.' . $photo->getClientOriginalExtension();
 
         // Initialize variable in upload images
         $rand = \Carbon\Carbon::parse()->format('Y-m-d') . '/' . rand();
-        $this->pathImageTemp = 'user/images/profile/';
+        $this->pathImageTemp = 'admin/user/images/profile/';
         $this->pathImage = $request->username . '/' . $rand . '/';
         $image_quality = ['original', 'thumb'];
-    
+
         foreach ($image_quality as $value) {
             $fullPath = $this->pathImageTemp . $this->pathImage . $value . '/';
             // Generating save temp image
             $thumbImg = \Image::make($photo->getRealPath());
-            ( $value == 'thumb' ? $thumbImg = $thumbImg->resize(200, 200) : '');
-            
+            ( $value == 'thumb' ? $thumbImg = $this->resize($thumbImg) : '');
             // Make directory
-            $this->makeDirectory($fullPath);
-            $fileImage = $thumbImg->save($fullPath . $imagename);
+//            $this->makeDirectory($fullPath);
+//            $fileImage = $thumbImg->save($fullPath . $imagename);
+            $thumbImg->stream(); 
 //            // Save to storage disk
-            \Storage::disk('public')->put($fullPath . $imagename, $fileImage->__toString());
+            \Storage::disk('public')->put($fullPath . $imagename, $thumbImg);
 //            // Path quality
             $this->pathQuality[$value] = $this->pathImage . $value . '/' . $imagename;
         }
     }
-    
-    protected function resize(){
-            
+
+    protected function resize($image) {
+        $image->resize($this->widthSize, $this->heightSize, function ($contraint) {
+            $contraint->aspectRatio();
+            $contraint->upsize();
+        });
+        return $this->backgroundCanvas->insert($image, 'center');
     }
 
     protected function makeDirectory($pathImage) {
-       
+
         File::exists($pathImage, 0777) or
                 File::makeDirectory($pathImage, 0777, true);
     }
